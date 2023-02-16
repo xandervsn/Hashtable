81a8c75bdc5f34cd524a21c6c6484336eb9208bb
U-BSD\412237 <412237@SHS-DGCSCS2.beaverton.k12.or.us>
2023-02-16 14:30:00 -0800

    randomizer

--- main.cpp
+++ main.cpp
@@ -1,71 +1,107 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cmath>
+#include <typeinfo>
 #include "node.h"
 
 using namespace std;
 
-int hash_func(char*, int);
+int hashf(int, int);
+int getI(int&);
 
 int main(){
   int HASH_LENGTH = 122;
-  Node* hashmap[HASH_LENGTH];
-  cout << "Input command (ADD, DLT, QUIT, PRINT)" << endl;
-  char input[10];
-  cin >> input;
-
-  if(strcmp(input, "ADD") == 0){
-    cout << "Add by user input (INPUT) or randomization (RAND)?" << endl;
-    cin >> input;
-    if(strcmp(input, "INPUT") == 0){
-      cout << "Input ID:" << endl;
-      cin >> input;
-      int id = (input[5]-'0') + (input[4]-'0')*10 + (input[3]-'0')*100 +
-	(input[2]-'0')*1000 + (input[1]-'0')*10000 + (input[0]-'0')*100000;
-      int pos = hash_func(input, HASH_LENGTH);
-
-      cout << "Input First Name:" << endl;
-      cin >> input;
-      
-      cout << "Input Last Name:" << endl;
-      cin >> input;
-      
-      cout << "Input GPA:" << endl;
-      cin >> input;
-    }else if(strcmp(input, "RAND") == 0){
-      ifstream firstFile;
-      firstFile.open("firstname.txt");
-      char arr[30][105];
-      int i = 0;
-      while(firstFile.getline(arr[i],100,' ')){
-	cout << arr[i] << endl;
-	cout << i << endl;
-	i++;
-      }
-      srand((unsigned) time(NULL));
-      int random = rand() % 49;
-      cout << arr[random] << endl;
-
-      ifstream lastFile;
-      lastFile.open("lastnames.txt");
-      char arr2[30][105];
-      i = 0;
-      while(lastFile.getline(arr2[i],100,' ')){
-        cout << arr2[i] << endl;
-        cout << i << endl;
-        i++;
-      }
-      random = rand() % 49;
-      cout << arr2[random] << endl;
-
-    }
-  }
+  Node** hashmap = new Node*[HASH_LENGTH];
+  int ii = 0;
   
+  while(true){
+    cout << "Input command (ADD, DLT, QUIT, PRINT)" << endl;
+    char input[10];
+    cin >> input;
+
+    if(strcmp(input, "ADD") == 0){
+      cout << "Add by user input (INPUT) or randomization (RAND)?" << endl;
+      cin >> input;
+      if(strcmp(input, "INPUT") == 0){
+	cout << "Input ID:" << endl;
+	cin >> input;
+	int id = (input[5]-'0') + (input[4]-'0')*10 + (input[3]-'0')*100 +
+	  (input[2]-'0')*1000 + (input[1]-'0')*10000 + (input[0]-'0')*100000;
+	
+	cout << "Input First Name:" << endl;
+	char first[35];
+	cin >> first;
+      
+	cout << "Input Last Name:" << endl;
+	char last[35];
+	cin >> last;
+
+	cout << "Input GPA:" << endl;
+	float gpa = 0.00;
+	cin >> gpa;
+
+	Node* newStudent = new Node(new Student((char*)first, last, id, gpa));
+	hashmap[hashf(id, HASH_LENGTH)] = newStudent;
+	cout << hashmap[hashf(id, HASH_LENGTH)]->getStudent()->gpa << endl;
+	cout << hashmap[3] << endl;
+      }else if(strcmp(input, "RAND") == 0){
+	//gen 1st name
+	ifstream firstFile;
+	firstFile.open("firstname.txt");
+	char arr[30][105];
+	int i = 0;
+	while(firstFile.getline(arr[i],100,' ')){
+	  i++;
+	}
+	srand((unsigned) time(NULL));
+	int random = rand() % 49;
+	char* first = arr[random];
+
+	//gen last name
+	ifstream lastFile;
+	lastFile.open("lastnames.txt");
+	char arr2[30][105];
+	i = 0;
+	while(lastFile.getline(arr2[i],100,' ')){
+	  i++;
+	}
+	random = rand() % 49;
+	char* last = arr2[random];
+
+	//gen gpa
+	double randF = (double)rand();
+	double gpa = fmod(randF, 4.99);
+		
+	int id = getI(ii);
+	cout << "ID:" << id << endl;
+	cout << "1" << endl;
+	int pos = hashf(id, HASH_LENGTH);
+	cout << "2" << endl;
+	Node* newStudent = new Node(new Student((char*)first, last, id, gpa));
+	cout << "3" << endl;
+	cout << hashmap[3] << endl;
+	//hashmap[hashf(id, HASH_LENGTH)] = newStudent;
+	cout << "4" << endl;
+      }
+    }
+  }  
   return 0;
 }
 
-int hash_func(char* id, int HASH_LENGTH){
+int hashf(int id, int HASH_LENGTH){
+  return id % HASH_LENGTH;
+}
+
+int getI(int& i){
+  i++;
+  return i;
+}
+
+/*
+int hashf(char* id, int HASH_LENGTH){
   //hashes input (with too much int division)
   return ((id[0] - '0') + (id[1] - '0') + (id[2] - '0') +
 	  (id[3] - '0') + (id[4] - '0') + (id[5] - '0')) * (id[5] - '0') / (HASH_LENGTH/25);
 }
+*/
--- node.cpp
+++ node.cpp
@@ -10,4 +10,28 @@ Student::Student(char* first, char* last, int id, float gpa){
   this->gpa = gpa;
 }
 
+// Get next node
+Node* Node::getNext() {
+  return next;
+}
 
+// Get student from node 
+Student* Node::getStudent() {
+  return student;
+}
+
+// Set next node
+void Node::setNext(Node* n) {
+  next = n;
+}
+
+// Node constructor
+Node::Node(Student* s) {
+  student = s;
+  next = nullptr;
+}
+
+// Node destructor
+Node::~Node() {
+  delete student; // Make sure we get rid of the student too 
+}
