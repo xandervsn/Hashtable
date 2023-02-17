f482a0992a21835374ad257b5a08dc65a1e05211
xandervsn <xandervsn@gmail.com>
2023-02-17 06:00:24 +0000

    add, print

--- main.cpp
+++ main.cpp
@@ -9,10 +9,17 @@ using namespace std;
 
 int hashf(int, int);
 int getI(int&);
+void add(Node** hashmap, Node* newStudent, int);
+void print(Node** hashmap, Node* student);
 
 int main(){
-  int HASH_LENGTH = 122;
+  int HASH_LENGTH = 100;
+  //Node* hashmap[HASH_LENGTH];
   Node** hashmap = new Node*[HASH_LENGTH];
+  for(int i = 0; i < HASH_LENGTH; i++){
+    hashmap[i] = NULL;
+  }
+  cout << hashmap[3] << endl;
   int ii = 0;
   
   while(true){
@@ -41,52 +48,69 @@ int main(){
 	float gpa = 0.00;
 	cin >> gpa;
 
+	int pos = hashf(id, HASH_LENGTH);
 	Node* newStudent = new Node(new Student((char*)first, last, id, gpa));
-	hashmap[hashf(id, HASH_LENGTH)] = newStudent;
-	cout << hashmap[hashf(id, HASH_LENGTH)]->getStudent()->gpa << endl;
-	cout << hashmap[3] << endl;
+	add(hashmap, newStudent, pos);
       }else if(strcmp(input, "RAND") == 0){
 	//gen 1st name
 	ifstream firstFile;
 	firstFile.open("firstname.txt");
-	char arr[30][105];
+	char arr[30][55];
 	int i = 0;
-	while(firstFile.getline(arr[i],100,' ')){
+	while(firstFile.getline(arr[i],50,' ')){
 	  i++;
 	}
 	srand((unsigned) time(NULL));
 	int random = rand() % 49;
 	char* first = arr[random];
-
+	
 	//gen last name
 	ifstream lastFile;
 	lastFile.open("lastnames.txt");
-	char arr2[30][105];
 	i = 0;
-	while(lastFile.getline(arr2[i],100,' ')){
+	while(lastFile.getline(arr[i],50,' ')){
 	  i++;
 	}
 	random = rand() % 49;
-	char* last = arr2[random];
+	char* last = arr[random];
 
+	cout << first << " " << last << endl;
+	
 	//gen gpa
 	double randF = (double)rand();
 	double gpa = fmod(randF, 4.99);
 		
 	int id = getI(ii);
-	cout << "ID:" << id << endl;
-	cout << "1" << endl;
 	int pos = hashf(id, HASH_LENGTH);
-	cout << "2" << endl;
 	Node* newStudent = new Node(new Student((char*)first, last, id, gpa));
-	cout << "3" << endl;
-	cout << hashmap[3] << endl;
-	//hashmap[hashf(id, HASH_LENGTH)] = newStudent;
-	cout << "4" << endl;
+	add(hashmap, newStudent, pos);
       }
+    }else if(strcmp(input, "PRINT") == 0){
+      cout << "Enter student ID:" << endl;
+      int id = 0;
+      cin >> id;
+      int pos = hashf(id, HASH_LENGTH);
+      print(hashmap, hashmap[pos]);
     }
   }  
   return 0;
+}
+
+void print(Node** hashmap, Node* current){
+  Student* student = current->getStudent();
+  cout << student->id << " " << student->first << " " << student->last << " " << student->gpa << endl;
+   if(current->getNext() != NULL){
+     print(hashmap, current->getNext());
+   }
+}
+
+void add(Node** hashmap, Node* newStudent, int pos){
+  newStudent->setNext(NULL);
+  if(hashmap[pos] == NULL){
+    hashmap[pos] = newStudent;
+  }else{
+    hashmap[pos]->setNext(newStudent);
+  }
 }
 
 int hashf(int id, int HASH_LENGTH){
